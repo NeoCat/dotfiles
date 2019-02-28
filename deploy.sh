d203543a0b9c6a6c3a6134ece7c0d7b7a6fe6d0f
NeoCat <neocat@neocat.jp>
2019-03-01 04:11:16 +0900

    Add tmux theme for powerline

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd $HOME
 ln -s .dotfiles/.{emacs,elisp,tmux.conf,zshrc} .
+# requires netifaces and psutil installation with pip
+mkdir -p ~/.config/powerline/themes/tmux
+ln -s $HOME/.dotfiles/powerline-themes-tmux-default.json ~/.config/powerline/themes/tmux/default.json
