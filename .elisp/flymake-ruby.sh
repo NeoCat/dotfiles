58b76a8918f2a40040ccd3aa962f703f94b93369
NeoCat <neocat@neocat.jp>
2019-07-26 12:58:51 +0900

    flymake-ruby: change priority

--- .elisp/flymake-ruby.sh
+++ .elisp/flymake-ruby.sh
@@ -5,22 +5,24 @@ exists () {
     which "$1" >/dev/null 2>&1
 }
 
-# use rubocop directly
-exists rubocop && exec $RUBOCOP "$@"
-
 # use rubocop in the bundle
 CWD="$PWD"
 abspath="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 until [ "$PWD" = "/" ]; do
     if [ -f Gemfile.lock ]; then
 	if grep rubocop Gemfile.lock >/dev/null; then
-	    exec bundle exec $RUBOCOP $abspath 2>&1
+	    opt=""
+	    [ -f .rubocop.yml ] && opt="-c .rubocop.yml"
+	    exec bundle exec $RUBOCOP $opt $abspath 2>&1
 	fi
 	break
     fi
     cd ..
 done
 cd "$CWD"
+
+# use rubocop directly
+exists rubocop && exec $RUBOCOP "$@"
 
 # if no rubocop is available, just use ruby syntax check
 ruby -c "$@"
