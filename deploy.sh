7a3fea2ed376e9f1c94594b05e91a4de06581633
NeoCat <neocat@neocat.jp>
2022-03-06 22:02:00 +0900

    Add gitconfig_default

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ ln -s .dotfiles/.{emacs,elisp,tmux.conf,zshrc} .
 # requires netifaces and psutil installation with pip
 mkdir -p ~/.config/powerline/themes/tmux
 ln -s $HOME/.dotfiles/powerline-themes-tmux-default.json ~/.config/powerline/themes/tmux/default.json
+cp -i .dotfiles/gitconfig_default .gitconfig
